bc7552240c21afcea1039fdebd709ddcd84f8887
haryfun <hary.vai@gmail.com>
2020-08-18 19:37:47 +0700

    add some function utilities

---
+++ README.md
@@ -0,0 +1,49 @@
+# q-utils 
+Npm package utilities for survival.
+
+## How To Use
+
+``` npm install q-utils --save ```
+
+
+if the value is: 
+```
+null, undefined, 0, ''. 
+```
+then the return is ```true``` and vice versa
+
+```javascript 
+// <Import the function> using es5
+const { isEmpty } = require('q-utils')
+
+// <Import the function> using es6
+import { isEmpty } from 'q-utils'
+
+// <Examples with number>
+
+// Will return false
+console.log(`Check Value 1 result is ${isEmpty(1)}`);
+// Will return true
+console.log(`Check Value 0 result is ${isEmpty(0)}`);
+
+// <Example with string>
+
+// Will return false
+console.log(`Check Value "hacktoberfest" result is ${isEmpty("hacktoberfest")}`);
+// Will return true
+console.log(`Check Value "" result is ${isEmpty("")}`);
+
+// <Example with array>
+
+// Will return false
+console.log(`Check Value [1,2,3,4,5] result is ${isEmpty([1,2,3,4,5])}`);
+// Will return true
+console.log(`Check Value [] result is ${isEmpty([])}`);
+
+// <Example with object>
+
+// Will return false
+console.log(`Check Value {name: "Udin"} result is ${isEmpty({name: "Udin"})}`);
+// Will return true
+console.log(`Check Value {} result is ${isEmpty({})}`);
+```
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+
+export const convertToRp = (angka: number, currency: string = 'IDR') => {
+   let value = angka.toString().split(".")[0];
+   const space = " ";
+   let rupiah = "";
+   let angkarev = value.toString().split("").reverse().join("");
+   for (let i = 0; i < angkarev.length; i++)
+      if (i % 3 === 0) rupiah += angkarev.substr(i, 3) + ".";
+   return (
+      currency + space +
+      rupiah
+         .split("", rupiah.length - 1)
+         .reverse()
+         .join("")
+   );
+};
+
+export const titleCase = (str: string, char: string) => {
+   let str_replace = str.toLowerCase().split(char);
+   return str_replace
+      .map((item: string) => item.charAt(0).toUpperCase() + item.slice(1))
+      .join(" ");
+};
+
+export const isEmpty = (val: any) => {
+   if (val === undefined) return true;
+
+   if (
+      typeof val == "function" ||
+      typeof val == "number" ||
+      typeof val == "boolean" ||
+      Object.prototype.toString.call(val) === "[object Date]"
+   )
+      return false;
+
+   if (val == null || val.length === 0)
+      // null or 0 length array
+      return true;
+
+   if (typeof val == "object") {
+      // empty object
+      let r = true;
+
+      // eslint-disable-next-line no-unused-vars
+      for (let _f in val) r = false;
+      return r;
+   }
+   return false;
+};
+
+// export const sorting = (value) => {
+//    const { sortColumn, sortType, data = [] } = value;
+
+//    if (data.length && sortColumn && sortType) {
+//       data.sort((a, b) => {
+//          let x = a[sortColumn];
+//          let y = b[sortColumn];
+//          if (typeof x === "string") {
+//             x = x.charCodeAt();
+//          }
+//          if (typeof y === "string") {
+//             y = y.charCodeAt();
+//          }
+//          if (sortType === "asc") {
+//             return x - y;
+//          } else {
+//             return y - x;
+//          }
+//       });
+//    }
+//    return data;
+// };
